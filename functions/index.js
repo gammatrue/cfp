1ea4a2f166ef99435a8ee8ea2b66c64366804d55
gammatrue <56622795+gammatrue@users.noreply.github.com>
2022-08-16 12:39:15 +0800

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-const doh = 'https://security.cloudflare-dns.com/dns-query'
+const doh = '1.1.1.1'
 const dohjson = 'https://security.cloudflare-dns.com/dns-query'
 const contype = 'application/dns-message'
 const jstontype = 'application/dns-json'
